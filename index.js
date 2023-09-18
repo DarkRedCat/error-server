795cd7e4fbc67d39d63b4abfb4a2d6e1d565cec6
DarkRedCat <85447298+DarkRedCat@users.noreply.github.com>
2023-09-18 11:20:57 +0300

    11

--- index.js
+++ index.js
@@ -40,18 +40,8 @@ app.get('/api/v1/books', async (req, res) => {
 });
 app.get('/api/v1/books1', async (req, res) => {
   try {
-    const data = await BookModel.find(query)
-      .skip(skip)
-      .limit(limit)
-      .sort({ [orderBy]: sortBy });
-    const totalItems = await BookModel.countDocuments(query);
     return res.status(200).json({
       msg: 'Ok1',
-      data,
-      totalItems,
-      totalPages: Math.ceil(totalItems / limit),
-      limit: +limit,
-      currentPage: page,
     });
   } catch (error) {
     return res.status(500).json({
