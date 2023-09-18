f23a277387317c93c654a0fcf43a3acade768798
DarkRedCat <85447298+DarkRedCat@users.noreply.github.com>
2023-09-18 11:46:35 +0300

    1211

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -52,11 +52,8 @@ const router = express.Router({ mergeParams: true });
 
 router.get('/', async (req, res) => {
   try {
-    // const list = await Category.find();
-    // res.status(200).send(list);
-    res.status(200).json({
-      message: '1',
-    });
+    const list = await Category.find();
+    res.status(200).send(list);
   } catch (e) {
     res.status(500).json({
       message: 'На сервере произошла ошибка. Попробуйте позже',
