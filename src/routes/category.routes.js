57deb8c59f216956525e30028a5057d56d5042c9
DarkRedCat <85447298+DarkRedCat@users.noreply.github.com>
2023-09-18 12:00:51 +0300

    test_1

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,53 +2,53 @@ const express = require('express');
 const Category = require('../models/Category');
 const router = express.Router({ mergeParams: true });
 
-// router.post('/:NewCategory', async (req, res) => {
-//   try {
-//     const { NewCategory } = req.params;
-//     if (NewCategory) {
-//       const listCategory = Category.find();
-//       await listCategory[0].Category.push(NewCategory);
-//       const updateArr = await Category.update({
-//         ...listCategory[0]._doc,
-//         Category: listCategory[0].Category,
-//       });
-//       res.send([updateArr]).status(201);
-//     } else {
-//       res.status(401).json({ message: 'Unauthorized' });
-//     }
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json({
-//       message: e,
-//     });
-//   }
-// });
-// router.patch('/:delCategory', async (req, res) => {
-//   try {
-//     const { delCategory } = req.params;
-//     if (delCategory) {
-//       const listCategory = await Category.find();
-//       const newArr = await listCategory[0].Category.filter(
-//         (c) => c !== delCategory
-//       );
-//       q;
+router.post('/:NewCategory', async (req, res) => {
+  try {
+    const { NewCategory } = req.params;
+    if (NewCategory) {
+      const listCategory = Category.find();
+      await listCategory[0].Category.push(NewCategory);
+      const updateArr = await Category.update({
+        ...listCategory[0]._doc,
+        Category: listCategory[0].Category,
+      });
+      res.send([updateArr]).status(201);
+    } else {
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: e,
+    });
+  }
+});
+router.patch('/:delCategory', async (req, res) => {
+  try {
+    const { delCategory } = req.params;
+    if (delCategory) {
+      const listCategory = await Category.find();
+      const newArr = await listCategory[0].Category.filter(
+        (c) => c !== delCategory
+      );
+      q;
 
-//       const updateArr = await Category.update({
-//         ...listCategory[0]._doc,
-//         Category: newArr,
-//       });
+      const updateArr = await Category.update({
+        ...listCategory[0]._doc,
+        Category: newArr,
+      });
 
-//       res.send([updateArr]).status(201);
-//     } else {
-//       res.status(401).json({ message: 'Unauthorized' });
-//     }
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json({
-//       message: e,
-//     });
-//   }
-// });
+      res.send([updateArr]).status(201);
+    } else {
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: e,
+    });
+  }
+});
 
 router.get('/', async (req, res) => {
   try {
