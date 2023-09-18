9e3762993546b8c44b344d8c33b767e0fac86406
DarkRedCat <85447298+DarkRedCat@users.noreply.github.com>
2023-09-18 15:16:25 +0300

    121212

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ connectDB();
 //**red */
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
